051feec11243e91f66791714f72426ad17a6ee99
준준 <166536663+junjuny0227@users.noreply.github.com>
2024-06-12 09:08:51 +0900

    Update main.c

--- main.c
+++ main.c
@@ -14,21 +14,18 @@
 #define ENTER 13
 #define BACKSPACE 8
 
-void setcolor(int color)
-{
+void setcolor(int color) {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 
-void nocursorview()
-{
+void nocursorview() {
     CONSOLE_CURSOR_INFO cursorInfo = { 0, };
     cursorInfo.dwSize = 1; // 커서 굵기 (1 ~ 100)
     cursorInfo.bVisible = FALSE; // 커서 Visible TRUE(보임) FALSE(숨김)
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
 }
 
-void yescursorview()
-{
+void yescursorview() {
     CONSOLE_CURSOR_INFO cursorInfo = { 0, };
     cursorInfo.dwSize = 1; // 커서 굵기 (1 ~ 100)
     cursorInfo.bVisible = TRUE; // 커서 Visible TRUE(보임) FALSE(숨김)
